50f404d008d2e82a4269340a6d3e31a12eb8a6d9
Andrey Prokopenko <prokopenkoav@ornl.gov>
2020-06-23 16:50:36 -0400

    Do not sort predicates

--- src/search.C
+++ src/search.C
@@ -377,10 +377,9 @@ findOBB(xsearch,obq->xc,obq->dxc,obq->vec,nsearch);
 
   Kokkos::View<int *, DeviceType> offset("offset", 0);
   Kokkos::View<int *, DeviceType> indices("indices", 0);
-  // We really don't need any buffer, but the logic in ArborX right now
-  // dictates that it has to be positive to avoid first pass
   bvh.query(queries, MyCallback{this, xsearch, donorId, donorId_helper, found_ptr},
-            indices, offset, 1 /*buffer_size*/);
+            indices, offset, ArborX::Experimental::TraversalPolicy().setPredicateSorting(false));
+
 
   for (i = 0; i < nsearch; i++) {
       if (i != xtag[i]) {
